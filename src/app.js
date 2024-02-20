7bd926ec0ba45b94174089a8b10dafd9a9207626
vishal Karale <vishalkaralevsk@gmail.com>
2024-02-20 09:15:13 +0530

    push

---
+++ src/app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const path = require("path");
+
+const folderPath = path.join(__dirname, "textFiles");
+
+// Middleware to create the textFiles folder if it doesn't exist
+if (!fs.existsSync(folderPath)) {
+  fs.mkdirSync(folderPath);
+}
+
+// Endpoint to create a text file with current timestamp
+app.post("/createFile", (req, res) => {
+  const timestamp = Date.now();
+  const fileName = `${timestamp}.txt`;
+  const filePath = path.join(folderPath, fileName);
+  fs.writeFile(filePath, timestamp.toString(), (err) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.send("File created successfully");
+  });
+});
+
+// Endpoint to retrieve all text files
+app.get("/getAllFiles", (req, res) => {
+  fs.readdir(folderPath, (err, files) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.send(files);
+  });
+});
+
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
