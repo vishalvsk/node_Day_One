f605e28bde75532db2b831edea19c03b51ae0ea6
vishal Karale <vishalkaralevsk@gmail.com>
2024-03-16 18:09:32 +0530

    push

--- src/app.js
+++ src/app.js
@@ -41,7 +41,7 @@ app.get("/getAllFiles", (req, res) => {
 });
 
 // Start the server
-const PORT = 3000;
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
